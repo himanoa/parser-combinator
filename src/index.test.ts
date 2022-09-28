f4d39d7fb6ea286f6e00d911eb22f8f40c54ca4a
himanoa <matsunoappy@gmail.com>
2022-09-28 14:20:34 +0900

    Implemented basic matcher

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,57 @@
-import { sum } from './index'
+import { anyChar, eof } from './index'
 
-describe("adfasdf", () => {
-  test("1 + 1 = 2", () => {
-    expect(sum(1,1)).toBe(2)
+describe("anyChar", () => {
+  test("success", () => {
+    const text = 'abc'
+    expect(anyChar({ text, rest: text, position: 0 })).toStrictEqual({
+      kind: 'success',
+      value: 'a',
+      context: {
+        text,
+        rest: 'bc',
+        position: 1
+      }
+    })
+  })
+
+  test("failed", () => {
+    const text = ''
+    expect(anyChar({ text, rest: text, position: 0 })).toStrictEqual({
+      kind: 'error',
+      expected: 'empty character',
+      context: {
+        text,
+        rest: '',
+        position: 0
+      }
+    })
+  })
+})
+
+describe("eof", () => {
+  test("success", () => {
+    const text = ""
+    expect(eof({ text, rest: text, position: 0 })).toStrictEqual({
+      kind: 'success',
+      value: null,
+      context: {
+        text,
+        rest: text,
+        position: 0
+      }
+    })
+  })
+
+  test("failed", () => {
+    const text = "a"
+    expect(eof({ text, rest: text, position: 0 })).toStrictEqual({
+      kind: 'error',
+      expected: 'not EOF',
+      context: {
+        text,
+        rest: text,
+        position: 0
+      }
+    })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,58 @@
-export const sum = (a: number, b: number): number => {
-  return a + b
+type Parser<T> = (ctx: Context) => Result<T>
+
+type Result<T> = Success<T> | Failure
+
+type Success<T> = {
+  kind: 'success',
+  value: T,
+  context: Context
+}
+
+type Failure = {
+  kind: 'error',
+  expected: string,
+  context: Context
+}
+
+type Context = {
+  text: string,
+  rest: string
+  position: number
+}
+
+const success = <T>(ctx: Context, value: T, rest: string, consumeCount: number): Success<T> => {
+  return {
+    value,
+    kind: 'success',
+    context: {
+      text: ctx.text,
+      rest: rest,
+      position: ctx.position + consumeCount
+    }
+  }
+}
+
+const failure = (ctx: Context, expected: string): Failure => {
+  return {
+    expected,
+    kind: 'error',
+    context: ctx
+  }
+}
+
+export const anyChar: Parser<string> = (ctx) => {
+  const [char, ...rest] = ctx.rest
+  return char != null ? success(ctx, char, rest.join(""), 1) : failure(ctx, "empty character")
+}
+
+export const eof: Parser<null> = (ctx) => {
+  if(ctx.text.length === ctx.position) {
+    return success(ctx, null, ctx.rest, 0)
+  }
+  return failure(ctx, "not EOF")
+}
+
+export const char: <T extends string[0]>(c: T) => Parser<T> = (c) => (ctx) => {
+  const [char, ...rest] = ctx.rest
+  return char === c ? success(ctx, c, rest.join(""), 1) : failure(ctx, `${char} is not ${c}`)
 }

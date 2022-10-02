50f633ecf7b52ed8a913a0b38d4d470a0eb265f1
himanoa <matsunoappy@gmail.com>
2022-10-02 22:42:34 +0900

    Add buildscript

---
+++ scripts/build.ts
@@ -0,0 +1,19 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt@0.31.0/mod.ts";
+
+const packageJson = Deno.readTextFileSync(new URL(import.meta.resolve('../package.json')))
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/index.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: JSON.parse(packageJson)
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
